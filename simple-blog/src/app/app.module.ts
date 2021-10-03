ed63c46d64b6be0d72e68410cd207f95ab10efc4
mamohamed252 <ma.mohamed252@gmail.com>
2021-10-02 19:28:27 -0500

    Added test for login-dialog and refactored imports

--- simple-blog/src/app/app.module.ts
+++ simple-blog/src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { MatButtonModule } from '@angular/material/button';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatInputModule } from '@angular/material/input';
-import { MatIconModule } from '@angular/material/icon';
 import { MatDialogModule } from '@angular/material/dialog'
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
 
 import { AppComponent } from './app.component';
 import { BlogNavbarComponent } from './blog-navbar/blog-navbar.component';
--- simple-blog/src/app/blog-dialogs/login-dialog/login-dialog.component.spec.ts
+++ simple-blog/src/app/blog-dialogs/login-dialog/login-dialog.component.spec.ts
@@ -1,4 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AppComponent } from '../../app.component';
+import { BlogNavbarComponent } from '../../blog-navbar/blog-navbar.component';
+import { MatDialogModule } from '@angular/material/dialog'
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { LoginDialogComponent } from './login-dialog.component';
 
@@ -8,7 +13,17 @@ describe('LoginDialogComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LoginDialogComponent ]
+      declarations: [ 
+        AppComponent,
+        BlogNavbarComponent,
+        LoginDialogComponent
+      ],
+      imports: [
+        MatDialogModule,
+        MatToolbarModule,
+        RouterTestingModule
+        
+      ],
     })
     .compileComponents();
   });
@@ -21,5 +36,12 @@ describe('LoginDialogComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should find submitLogin button', () => {
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement;
+    const loginButton = compiled.querySelector('#login-form-btn');
+    expect(loginButton).toBeTruthy;
   });
 });
