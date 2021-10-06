cd63042cfcfc3f89dbbc6628025ed3f4e74a8994
InfAurora <albajo0123@gmail.com>
2021-10-06 18:59:00 -0500

    Add home page to routes, link home logo to home route

--- simple-blog/src/app/app-routing.module.ts
+++ simple-blog/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BlogHomePageComponent } from './blog-home-page/blog-home-page.component';
 import { BlogProfilePageComponent } from './blog-profile-page/blog-profile-page.component';
 
 const routes: Routes = [
   { path: 'profile', component: BlogProfilePageComponent },
+  { path: '/', component: BlogHomePageComponent },
 ];
 
 @NgModule({
