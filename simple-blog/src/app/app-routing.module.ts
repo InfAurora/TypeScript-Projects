a171d6980126536be7eed542e79b96e70cdefbde
InfAurora <albajo0123@gmail.com>
2021-10-06 18:55:50 -0500

    Add profile page to routes, link profile button to profile route

--- simple-blog/src/app/app-routing.module.ts
+++ simple-blog/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BlogProfilePageComponent } from './blog-profile-page/blog-profile-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'profile', component: BlogProfilePageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
